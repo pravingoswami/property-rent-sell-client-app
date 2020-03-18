7ef8a3b1272bebdd376ce167afbc4725e04472bb
pravingoswami <goswami4737@gmail.com>
2020-03-18 15:08:07 +0530

    added all the links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,94 @@
-import React from 'react'
+import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBIcon, MDBContainer } from 'mdbreact';
+import { BrowserRouter as Router , Link, Route} from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import Commercial from './Components/commercial/Commercial';
+import HomePage from './Components/home/HomePage';
+import RentPage from './Components/rent/RentPage';
+import SellPage from './Components/sell/SellPage';
+import Management from './Components/management/Management';
+import ContactUs from './Components/contact-us/ContactUs';
 
-class App extends React.Component{
-  render(){
-    return(
+class NonFixedNavbarExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
+
+  linkStyle = {
+    color : "white",
+    textDecoration : "none"
+  }
+
+  render() {
+    const container = { height: 1300 }
+    return (
       <div>
-        <h1>hi</h1>
+        <Router>
+          <header>
+            <MDBNavbar color="special-color-dark" dark expand="md">
+              <MDBNavbarBrand >
+                <strong>BARKELEY SQUARE</strong>
+              </MDBNavbarBrand>
+              <MDBNavbarToggler onClick={this.onClick} />
+              <MDBCollapse isOpen={this.state.collapse} navbar>
+                <MDBNavbarNav left>
+                  <MDBNavItem >
+                    <MDBNavLink to="/home"><Link style = {this.linkStyle} to = "/home" >Home</Link></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="/rent"><Link style = {this.linkStyle} to = "/rent" >For Rent</Link></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                  <MDBNavLink to="/sell"><Link style = {this.linkStyle} to = "/sell" >For Rent</Link></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                  <MDBNavLink to="/management"><Link style = {this.linkStyle} to = "/management" >Management</Link></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                  <MDBNavLink to="/commercial"><Link style = {this.linkStyle} to = "/commercial" >Commercial</Link></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                  <MDBNavLink to="/contact-us"><Link style = {this.linkStyle} to = "/contact-us" >Contact Us</Link></MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="facebook-f" /></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="twitter" /></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="linkedin" /></MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </header>
+
+          <Route path = "/home" component = {HomePage} />
+          <Route path = "/rent" component = {RentPage} />
+          <Route path = "/sell" component = {SellPage} />
+          <Route path = "/management" component = {Management} />
+          <Route path = "/commercial" component = {Commercial} />
+          <Route path = "/contact-us" component = {ContactUs} />
+
+        </Router>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default NonFixedNavbarExample;
---
+++ src/Components/commercial/Commercial.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { MDBContainer } from 'mdbreact'
+
+function Commercial(props){
+    return(
+        <div>
+            <MDBContainer className="text-center my-5">
+                <h1 align = "center" >This is Commercial</h1>
+                <br></br>
+            <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis  aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia  deserunt mollit anim id est laborum.</p>
+          </MDBContainer>
+        </div>
+    )
+}
+
+export default Commercial
